bc08898dabf91c56380a189d3e0b9e0a8ae735b8
cardonero34 <produccionmultimediaemanuel@gmail.com>
2026-04-07 10:45:55 -0500

    estructuración de secciones del sitio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,147 @@
 import { Header } from "../components/Header"
-
-
+import "../stylesheets/Home.css"
 
 export const Home = () => {
     return (
         <>
-            <Header />
+            <div className="position-fixed w-100">
+                <Header />
+            </div>
+
+            <section className="portada">
+                <img className="img-fluid" src="/imgportada.jpg" alt="" />
+                <div className="position-absolute top-50 start-50 translate-middle w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <div>
+                        <p className="text-center">Leersé: una experiencia de lectura expandida que conecta emociones, pensamiento crítico y comunidad.</p>
+                    </div>
+                    <div className="w-50 d-flex gap-5">
+                        <button type="button" className="btn2 rounded-3">Explora la animación</button>
+                        <button type="button" className="btn3 rounded-3">Conoce más</button>
+                    </div>
+                </div>
+
+                <div className="w-100 position-absolute bottom-0 start-50 translate-middle-x">
+                    <svg className="separador w-100" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100.94">
+                        <path d="M700,0v100.94L298.06,36.68c-90.63-14.49-183.52,1.57-264.04,45.63L0,100.94V0h700Z" />
+                    </svg>
+                </div>
+
+            </section>
+
+            <section className="problema d-flex justify-content-center align-items-center">
+                <div className="w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <p>El problema que resolvemos</p>
+                    <p>En un mundo que nos empuja a la lectura superficial, muchos encuentran barreras emocionales, simbolicas y culturales que dificultan una conexión profunda con los libros. Leerse nace para ransformar esa experiencia</p>
+                </div>
+            </section>
+
+            <section className="propuesta position-relative d-flex justify-content-center align-items-center">
+                <div className="w-100 position-absolute top-0 start-50 translate-middle-x">
+                    <svg className="separador2 w-100" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100.94">
+                        <path d="M700,0v100.94L298.06,36.68c-90.63-14.49-183.52,1.57-264.04,45.63L0,100.94V0h700Z" />
+                    </svg>
+                </div>
+                <div className="w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <p>Nuestra propuesta ¿Qué es Leersé?</p>
+                    <p>Leersé es una plataforma cultural digital que reimagina la lectura, convirtiéndola en un viaje personal y colectivo. Fusionamos lo poético con lo crítico, lo íntimo con lo compartido, para que cada texto se convierta en una puerta a nuevas emociones y pensamientos.</p>
+                </div>
+                <div className="w-100 position-absolute bottom-0 start-50 translate-middle-x">
+                    <svg className="separador3 w-100" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100.94">
+                        <path d="M700,0v100.94L298.06,36.68c-90.63-14.49-183.52,1.57-264.04,45.63L0,100.94V0h700Z" />
+                    </svg>
+                </div>
+            </section>
+
+            <section className="funciones d-flex justify-content-center align-items-center">
+                <div className="w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <p>Nuestra propuesta ¿Qué es Leersé?</p>
+                    <p>Leersé es una plataforma cultural digital que reimagina la lectura, convirtiéndola en un viaje personal y colectivo. Fusionamos lo poético con lo crítico, lo íntimo con lo compartido, para que cada texto se convierta en una puerta a nuevas emociones y pensamientos.</p>
+                </div>
+            </section>
+
+            <section className="demo position-relative d-flex justify-content-center align-items-center">
+                <div className="w-100 position-absolute top-0 start-50 translate-middle-x">
+                    <svg className="separador4 w-100" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100.94">
+                        <path d="M700,0v100.94L298.06,36.68c-90.63-14.49-183.52,1.57-264.04,45.63L0,100.94V0h700Z" />
+                    </svg>
+                </div>
+                <div className="w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <p>Nuestra propuesta ¿Qué es Leersé?</p>
+                    <p>Leersé es una plataforma cultural digital que reimagina la lectura, convirtiéndola en un viaje personal y colectivo. Fusionamos lo poético con lo crítico, lo íntimo con lo compartido, para que cada texto se convierta en una puerta a nuevas emociones y pensamientos.</p>
+                </div>
+                <div className="w-100 position-absolute bottom-0 start-50 translate-middle-x">
+                    <svg className="separador5 w-100" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100.94">
+                        <path d="M700,0v100.94L298.06,36.68c-90.63-14.49-183.52,1.57-264.04,45.63L0,100.94V0h700Z" />
+                    </svg>
+                </div>
+            </section>
+
+            <section className="beneficios d-flex justify-content-center align-items-center">
+                <div className="w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <p>Nuestra propuesta ¿Qué es Leersé?</p>
+                    <p>Leersé es una plataforma cultural digital que reimagina la lectura, convirtiéndola en un viaje personal y colectivo. Fusionamos lo poético con lo crítico, lo íntimo con lo compartido, para que cada texto se convierta en una puerta a nuevas emociones y pensamientos.</p>
+                </div>
+            </section>
+
+            <section className="comunidad position-relative d-flex justify-content-center align-items-center">
+                <div className="w-100 position-absolute top-0 start-50 translate-middle-x">
+                    <svg className="separador6 w-100" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100.94">
+                        <path d="M700,0v100.94L298.06,36.68c-90.63-14.49-183.52,1.57-264.04,45.63L0,100.94V0h700Z" />
+                    </svg>
+                </div>
+                <div className="w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <p>Nuestra propuesta ¿Qué es Leersé?</p>
+                    <p>Leersé es una plataforma cultural digital que reimagina la lectura, convirtiéndola en un viaje personal y colectivo. Fusionamos lo poético con lo crítico, lo íntimo con lo compartido, para que cada texto se convierta en una puerta a nuevas emociones y pensamientos.</p>
+                </div>
+                <div className="w-100 position-absolute bottom-0 start-50 translate-middle-x">
+                    <svg className="separador7 w-100" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100.94">
+                        <path d="M700,0v100.94L298.06,36.68c-90.63-14.49-183.52,1.57-264.04,45.63L0,100.94V0h700Z" />
+                    </svg>
+                </div>
+            </section>
+
+            <section className="personajes d-flex justify-content-center align-items-center">
+                <div className="w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <p>Nuestra propuesta ¿Qué es Leersé?</p>
+                    <p>Leersé es una plataforma cultural digital que reimagina la lectura, convirtiéndola en un viaje personal y colectivo. Fusionamos lo poético con lo crítico, lo íntimo con lo compartido, para que cada texto se convierta en una puerta a nuevas emociones y pensamientos.</p>
+                </div>
+            </section>
+
+            <section className="actyrec position-relative d-flex justify-content-center align-items-center">
+                <div className="w-100 position-absolute top-0 start-50 translate-middle-x">
+                    <svg className="separador8 w-100" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100.94">
+                        <path d="M700,0v100.94L298.06,36.68c-90.63-14.49-183.52,1.57-264.04,45.63L0,100.94V0h700Z" />
+                    </svg>
+                </div>
+                <div className="w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <p>Nuestra propuesta ¿Qué es Leersé?</p>
+                    <p>Leersé es una plataforma cultural digital que reimagina la lectura, convirtiéndola en un viaje personal y colectivo. Fusionamos lo poético con lo crítico, lo íntimo con lo compartido, para que cada texto se convierta en una puerta a nuevas emociones y pensamientos.</p>
+                </div>
+                <div className="w-100 position-absolute bottom-0 start-50 translate-middle-x">
+                    <svg className="separador9 w-100" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100.94">
+                        <path d="M700,0v100.94L298.06,36.68c-90.63-14.49-183.52,1.57-264.04,45.63L0,100.94V0h700Z" />
+                    </svg>
+                </div>
+            </section>
+
+            <section className="registro d-flex justify-content-center align-items-center">
+                <div className="w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <p>Nuestra propuesta ¿Qué es Leersé?</p>
+                    <p>Leersé es una plataforma cultural digital que reimagina la lectura, convirtiéndola en un viaje personal y colectivo. Fusionamos lo poético con lo crítico, lo íntimo con lo compartido, para que cada texto se convierta en una puerta a nuevas emociones y pensamientos.</p>
+                </div>
+            </section>
+
+            <section className="footer position-relative d-flex justify-content-center align-items-center">
+                <div className="w-100 position-absolute top-0 start-50 translate-middle-x">
+                    <svg className="separador10 w-100" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100.94">
+                        <path d="M700,0v100.94L298.06,36.68c-90.63-14.49-183.52,1.57-264.04,45.63L0,100.94V0h700Z" />
+                    </svg>
+                </div>
+                <div className="w-75 d-flex flex-column justify-content-center align-items-center gap-4">
+                    <p>Nuestra propuesta ¿Qué es Leersé?</p>
+                    <p>Leersé es una plataforma cultural digital que reimagina la lectura, convirtiéndola en un viaje personal y colectivo. Fusionamos lo poético con lo crítico, lo íntimo con lo compartido, para que cada texto se convierta en una puerta a nuevas emociones y pensamientos.</p>
+                </div>
+            </section>
         </>
     )
 }
