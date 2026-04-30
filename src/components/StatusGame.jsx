d171128dd2e5a5c1baad747ce8dc4d4478f0fa41
cardonero34 <produccionmultimediaemanuel@gmail.com>
2026-04-30 11:31:02 -0500

    botones gemas

--- src/components/StatusGame.jsx
+++ src/components/StatusGame.jsx
@@ -5,10 +5,18 @@ export const StatusGame = () => {
   const [vidas, setVidas] = useState({})
 
   return (
-  <div className="vidas">
-    <button>Vida 1</button>
-    <button>Vida 2</button>
-    <button>Vida 3</button>
-  </div>
+    <>
+      <div className="gemas">
+        <button>gema 1</button>
+        <button>gema 2</button>
+        <button>gema 3</button>
+      </div>
+
+      <div className="vidas">
+        <button>Vida 1</button>
+        <button>Vida 2</button>
+        <button>Vida 3</button>
+      </div>
+    </>
   )
 }
